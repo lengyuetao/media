b1e181a4c548da400ac2f41064348122e59c01c2
zhangat <zhangantao>
2017-04-28 16:52:29 +0800

    test

--- src/main/java/com/tao/controller/IndexController.java
+++ src/main/java/com/tao/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -19,12 +20,26 @@ public class IndexController {
     @Autowired
     UserInfoService userInfoService;
     @RequestMapping("/index")
-    public String index(){
+    public String index(HttpServletRequest request,String userName){
         log.error("进入首页");
 //        List<UserInfo> list=userInfoService.findAllUserInfo();
 //        for(UserInfo userInfo:list){
 //          System.out.println("用户信息："+userInfo.toString());
 //        }
+
+        if(null==userName){
+            userName="123";
+        }
+        if(null==request.getSession().getAttribute("user")){
+            request.getSession().setAttribute("user",userName);
+            System.out.println("session设置成功");
+        }else {
+            System.out.println("已经登录成功,用户名："+request.getSession().getAttribute("user"));
+        }
+
+        System.out.println("user用户名："+request.getSession().getAttribute("user"));
+
+
         return "index";
     }
 }
