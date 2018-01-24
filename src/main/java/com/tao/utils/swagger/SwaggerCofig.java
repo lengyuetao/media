5c6dfec4c117ef5550fd32e1c14cd27e6413e3df
lengyuetao <dyadan@163.com>
2018-01-24 16:01:19 +0800

    注销文档Swagger配置

--- src/main/java/com/tao/utils/swagger/SwaggerCofig.java
+++ src/main/java/com/tao/utils/swagger/SwaggerCofig.java
@@ -1,40 +1,40 @@
-package com.tao.utils.swagger;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
-/**
- * Created by zhangantao on 2017/6/24.
- */
-@Configuration
-@EnableSwagger2
-public class SwaggerCofig {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("对外开放接口API 文档")
-                .description("HTTP对外开放接口")
-                .version("1.0.0")
-                .termsOfServiceUrl("http://xxx.xxx.com")
-                .license("LICENSE")
-                .licenseUrl("http://xxx.xxx.com")
-                .build();
-    }
-
-}
+//package com.tao.utils.swagger;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//
+///**
+// * Created by zhangantao on 2017/6/24.
+// */
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerCofig {
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .build()
+//                .apiInfo(apiInfo());
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("对外开放接口API 文档")
+//                .description("HTTP对外开放接口")
+//                .version("1.0.0")
+//                .termsOfServiceUrl("http://xxx.xxx.com")
+//                .license("LICENSE")
+//                .licenseUrl("http://xxx.xxx.com")
+//                .build();
+//    }
+//
+//}
