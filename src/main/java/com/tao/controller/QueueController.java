8490e7a67baab591221e27aa8b761593f411d09e
lengyuetao <dyadan@163.com>
2018-01-24 16:26:49 +0800

    注释redis消息队列

--- src/main/java/com/tao/controller/QueueController.java
+++ src/main/java/com/tao/controller/QueueController.java
@@ -1,6 +1,5 @@
 package com.tao.controller;
 
-import com.tao.utils.jms.SendMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/queue")
 public class QueueController {
 
-    @Autowired
-    private SendMessage sendMessage;
 
     @RequestMapping(value="/redis")
     public void redis(){
